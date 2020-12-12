ff1bd83b21b865093c23c9fb674e55a36cee9325
Michael Keller <michaelkeller3000@gmail.com>
2020-12-12 11:48:44 +0100

    Updated Welcome Message

--- app/App.tsx
+++ app/App.tsx
@@ -12,7 +12,6 @@ class App extends React.Component {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
-        <Text>Yo!</Text>
       </View>
     );
   }
