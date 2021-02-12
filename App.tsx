0037adcdb1f33c33096c31cfa2d3052dac304c2b
Michael <michaelkeller3000@gmail.com>
2021-02-12 11:24:54 +0100

    .

--- App.tsx
+++ App.tsx
@@ -78,7 +78,7 @@ export default function App() {
         userToken = null; //await AsyncStorage.getItem('auth-token');
       } catch (e) {
         // Restoring token failed
-        alert("Error retrieving token: "+ e);
+        alert("Error retrieving token:  "+ e);
       }
       dispatch({ type: "RESTORE_TOKEN", token: userToken });
     };
