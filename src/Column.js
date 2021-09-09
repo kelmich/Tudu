0d118987d617791a561c74c1d2fb4681adda0bc3
Michael Keller <michaelkeller3000@gmail.com>
2021-09-09 23:36:05 +0200

    Added First Layout attempt (days)

--- src/Column.js
+++ src/Column.js
@@ -1,15 +1,28 @@
+import "./Column.css";
+
 import Task from "./Task";
 import { Droppable } from "react-beautiful-dnd";
 
 function Column(props) {
   return (
-    <div>
+    <div className="Column">
       <h2>{props.column.title}</h2>
       <Droppable droppableId={props.column.id}>
         {
           // note: necessary because droppable expects a function & for styling later
-          (provided) => (
-            <div ref={provided.innerRef} {...provided.droppableProps}>
+          (provided, snapshot) => (
+            <div
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+              style={{
+                backgroundColor:
+                  snapshot.isDraggingOver &&
+                  !props.column.taskIds.includes(snapshot.draggingOverWith)
+                    ? "#2d5ec2"
+                    : "transparent",
+              }}
+              className="Tasklist"
+            >
               {props.tasks.map((task, index) => (
                 <Task key={task.id} task={task} index={index} />
               ))}
--- src/initial-data.js
+++ src/initial-data.js
@@ -1,18 +1,56 @@
 const initialData = {
-    tasks: {
-        "task-1": { id: "task-1", content: "Analysis Serie 1" },
-        "task-2": { id: "task-2", content: "Analysis Serie 2" },
-        "task-3": { id: "task-3", content: "Analysis Serie 3" },
-        "task-4": { id: "task-4", content: "Analysis Serie 4" },
+  tasks: {
+    "task-1": { id: "task-1", content: "Analysis Serie 1" },
+    "task-2": { id: "task-2", content: "Analysis Serie 2" },
+    "task-3": { id: "task-3", content: "Analysis Serie 3" },
+    "task-4": { id: "task-4", content: "Analysis Serie 4" },
+  },
+  columns: {
+    Monday: {
+      id: "Monday",
+      title: "Monday",
+      taskIds: ["task-1", "task-2", "task-3", "task-4"],
     },
-    columns: {
-        "column-1": {
-            id: "column-1",
-            title: "To Do",
-            taskIds: ["task-1", "task-2", "task-3", "task-4"]
-        },
+    Tuesday: {
+      id: "Tuesday",
+      title: "Tuesday",
+      taskIds: [],
     },
-    columnOrder: ["column-1"],
-}
+    Wednesday: {
+      id: "Wednesday",
+      title: "Wednesday",
+      taskIds: [],
+    },
+    Thursday: {
+      id: "Thursday",
+      title: "Thursday",
+      taskIds: [],
+    },
+    Friday: {
+      id: "Friday",
+      title: "Friday",
+      taskIds: [],
+    },
+    Saturday: {
+      id: "Saturday",
+      title: "Saturday",
+      taskIds: [],
+    },
+    Sunday: {
+      id: "Sunday",
+      title: "Sunday",
+      taskIds: [],
+    },
+  },
+  columnOrder: [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ],
+};
 
 export default initialData;
