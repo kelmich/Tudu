0fb1db78c141cd4dd27e429a7d26bf2dbe723753
Michael Keller <michaelkeller3000@gmail.com>
2021-12-26 16:24:59 +0100

    Changed Build Trigger

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,13 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Tudu</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome Michael to <a href="https://nextjs.org">Next.js</a> on Docker!
+          Welcome Michael to <a href="https://nextjs.org">Tudu</a> on Docker!
         </h1>
 
         <p className={styles.description}>
