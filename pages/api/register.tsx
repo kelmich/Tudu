25bddece13c3e665740245950893f1c17f143c79
Michael Keller <michaelkeller3000@gmail.com>
2021-12-28 13:19:52 +0100

    Testing env

--- pages/api/register.tsx
+++ pages/api/register.tsx
@@ -2,15 +2,15 @@ import { NextApiRequest, NextApiResponse } from "next";
 import db from "./db";
 
 export default async function hello(req: NextApiRequest, res: NextApiResponse) {
-  const docRef = db.collection("Users").doc(req.body.username);
-
-  console.log(process.env.PROJECT_ID);
-
-  // use create?
-  await docRef.set({
-    pubKey: req.body.pubKey,
-    encPrivKey: req.body.encPrivKey,
-  });
-
-  res.status(200).end();
+  try {
+    const docRef = db.collection("Users").doc(req.body.username);
+    // use create?
+    await docRef.set({
+      pubKey: req.body.pubKey,
+      encPrivKey: req.body.encPrivKey,
+    });
+    res.status(200).end();
+  } catch (error) {
+    res.status(400).json({ error: process.env });
+  }
 }
